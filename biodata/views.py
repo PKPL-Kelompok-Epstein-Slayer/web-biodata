07380d39ca00e7bc7793e2bdf70bd310142f117c
Muhathir . <muhathirmro@gmail.com>
2026-04-02 11:37:40 +0700

    add simple authorization

--- biodata/views.py
+++ biodata/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
 
-
 @require_GET
 def index(request):
-    # Fungsi ini bertugas memanggil file index.html yang kamu buat tadi
-    return render(request, "biodata/index.html")
+    allowed_emails = [
+        'muhammad.rafi41@ui.ac.id',
+        'bismazharfan@gmail.com',
+        'refkiseptian1@gmail.com',
+        'muhathirmro@gmail.com'
+    ]
+    
+    is_anggota = False
+    
+    if request.user.is_authenticated and request.user.email in allowed_emails:
+        is_anggota = True
+        
+    context = {
+        'is_anggota': is_anggota
+    }
+    
+    return render(request, "biodata/index.html", context)
